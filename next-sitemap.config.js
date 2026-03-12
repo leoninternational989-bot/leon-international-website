f4fa576a1ab9912305914120be1941ceb72c67e1
umm-e-habiba <ummehabiba989@gmail.com>
2026-03-12 11:06:33 +0500

    chore: exclude admin routes and image assets from sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,7 +7,14 @@ module.exports = {
     changefreq: 'weekly',
     priority: 0.7,
     sitemapSize: 5000,
-    exclude: ['/server-sitemap.xml'],
+    exclude: [
+        '/server-sitemap.xml',
+        '/admin',
+        '/admin/*',
+        '/apple-icon.png',
+        '/opengraph-image.png',
+        '/twitter-image.png',
+    ],
     transform: async (config, path) => {
         return {
             loc: path,
@@ -19,7 +26,7 @@ module.exports = {
     },
     robotsTxtOptions: {
         policies: [
-            { userAgent: '*', allow: '/' },
+            { userAgent: '*', allow: '/', disallow: ['/admin', '/admin/*'] },
             { userAgent: 'GPTBot', allow: '/' },
             { userAgent: 'ChatGPT-User', allow: '/' },
             { userAgent: 'Google-Extended', allow: '/' },
